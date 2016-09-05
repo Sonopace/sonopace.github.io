a9b3facb9f10d643077cf5f63d7388419d6c46fb
zotherstupidguy <zotherstupidguy@gmail.com>
2016-09-05 14:40:19 +0200

    adding website versions

--- sonoplayer/src/app/App.js
+++ sonoplayer/src/app/App.js
@@ -5,16 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>sonoplayer</h2>
-        </div>
+	<div className="App">
+	<div className="App-header">
+	<img src={logo} className="App-logo" alt="logo" />
+	<h2>sonoplayer</h2>
+	</div>
 	<p className="App-intro">
 	dd
-        </p>
-      </div>
-    );
+	</p>
+	</div>
+	);
   }
 }
 

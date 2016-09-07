3d8b471f683014dcf9f6cc3cd851f533c3fc0591
zotherstupidguy <zotherstupidguy@gmail.com>
2016-09-07 07:50:30 +0200

    songs_selector selects files and return a list of them

--- sonoplayer/src/components/songs_selector/songs_selector.js
+++ sonoplayer/src/components/songs_selector/songs_selector.js
@@ -1,46 +1,25 @@
 import React, { Component } from 'react';
-//import './select_songs.css';
-//import select_songs_local from './folder.svg';
-//import select_songs_remote from './cloud.svg';
-// <input className="" id='audio_file' type='file' accept='audio/*' />
-
-const getSongs = (songs, filter) => {
-  switch (filter) {
-    case 'EMPTY':
-      return todos
-    case 'SELECTED':
-	return todos.filter(t => t.completed)
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    songs: getSongs(state.songs, state.visibilityFilter)
-  }
-}
 
 class SongsSelector extends Component {
-  // 
-  //TODO use redux to update the state of the "playlist"
-  //
+
+  appendPlaylist(action){
+    console.log(name);
+    console.log("logging...");
+    console.log(this.props);
+    console.log(action.target.files);
+  }
+
   render() {
     return (
 	<div>
 	<h6> SELECT SONGS </h6>
-	<input type="file" id="files" name="files[]" accept='audio/*' multiple />
-	<a href=""  className="mdl-button mdl-shadow--2dp">
-	HardDrive	
-	</a>
+	<input type="file" id="input" name="files[]" accept="audio/*" multiple onChange={ this.appendPlaylist.bind(this) } />
 
 
-	</div>
-	);
-    //	<div>
-    //	<a href=""  className="mdl-button mdl-shadow--2dp">
-    //	Cloud
-    //	</a>
-    //	</div>
+	<div  className="mdl-button mdl-shadow--2dp" onClick={ this.appendPlaylist.bind(this) } > HardDrive </div>
+	</div>);
   }
-}
+
+};
 
 export default SongsSelector;
